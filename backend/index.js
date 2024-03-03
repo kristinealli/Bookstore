63b0792598cdcb0884750d5b5f9dc9f16b893a87
Kristine Johnson <kristineallisonjohnson@outlook.com>
2024-03-03 16:09:53 -0500

    React Frontend

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,14 @@ const app = express();
 app.use(express.json());
 
 //Middleware for handling CORS policy
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-        methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
-        allowedHeaders: ["Content-Type"],
-    })
-);
+app.use(cors());
+// app.use(
+//     cors({
+//         origin: "http://localhost:3000",
+//         methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
+//         allowedHeaders: ["Content-Type"],
+//     })
+// );
 
 app.get("/", (request, response) => {
 	console.log(request);
